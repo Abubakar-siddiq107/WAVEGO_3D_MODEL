b4f9103247b91476f74724d0c38f3608cfd51b25
Abubakar-siddiq107 <abubakarsiddiq.2005@outlook.com>
2026-06-22 15:24:31 +0530

    Update gazebo.launch.py

--- assemblyURDF_sim_v3/launch/gazebo.launch.py
+++ assemblyURDF_sim_v3/launch/gazebo.launch.py
@@ -4,57 +4,56 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def generate_launch_description():
 
-```
-package_name = "assemblyURDF_sim_v3"
+    package_name = "assemblyURDF_sim_v3"
 
-urdf_path = os.path.join(
-    get_package_share_directory(package_name),
-    "urdf",
-    "assemblyURDF_sim_v3.urdf"
-)
-
-world = "empty.sdf"
-
-return LaunchDescription([
-
-    # Start Gazebo Harmonic
-    ExecuteProcess(
-        cmd=[
-            "gz",
-            "sim",
-            "-r",
-            world
-        ],
-        output="screen"
-    ),
-
-    # Publish robot state
-    Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[
-            {
-                "robot_description":
-                open(urdf_path).read()
-            }
-        ],
-        output="screen"
-    ),
-
-    # Spawn robot into Gazebo
-    Node(
-        package="ros_gz_sim",
-        executable="create",
-        arguments=[
-            "-name",
-            "assemblyURDF_sim_v3",
-            "-file",
-            urdf_path
-        ],
-        output="screen"
+    urdf_path = os.path.join(
+        get_package_share_directory(package_name),
+        "urdf",
+        "assemblyURDF_sim_v3.urdf"
     )
 
-])
-```
+    with open(urdf_path, "r") as f:
+        robot_desc = f.read()
+
+    return LaunchDescription([
+
+        # Start Gazebo Harmonic
+        ExecuteProcess(
+            cmd=[
+                "gz",
+                "sim",
+                "-r",
+                "empty.sdf"
+            ],
+            output="screen"
+        ),
+
+        # Publish robot state
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[
+                {
+                    "robot_description": robot_desc
+                }
+            ],
+            output="screen"
+        ),
+
+        # Spawn robot into Gazebo
+        Node(
+            package="ros_gz_sim",
+            executable="create",
+            arguments=[
+                "-name",
+                "assemblyURDF_sim_v3",
+                "-file",
+                urdf_path
+            ],
+            output="screen"
+        )
+
+    ])
